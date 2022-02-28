bc31f05fa04fb02cc1eb0961306b1deb501aa5bd
Steven Woolston <steven.woolston@gmail.com>
2022-02-28 21:48:27 +1000

    Iterated version for production deployment

--- order-pop.php
+++ order-pop.php
@@ -1,11 +1,11 @@
 <?php
 /*
 @package OP_Developer
-@version 1.6
+@version 1.6.1
 Plugin Name: Order Pop
 Plugin URI: https://github.com/startsat60/order-pop
 Description: Woocommerce Order Pop Notification. Display previous orders to your customers to promote sales.
-Version: 1.6
+Version: 1.6.1
 Author: Woolston Web Design
 Author URI: https://www.woolston.com.au
 GitHub Plugin URI: https://github.com/startsat60/order-pop
@@ -47,14 +47,14 @@ function my_custom_favicon() {
 ';
 }
 function op_enqueue_assets() {
-    wp_enqueue_style('op-style', plugin_dir_url(__FILE__) . '/dist/css/order-pop.min.css', array(), '1.6', 'all');
+    wp_enqueue_style('op-style', plugin_dir_url(__FILE__) . '/dist/css/order-pop.min.css', array(), '1.6.1', 'all');
 
     $op_options = get_option('op-plugin')['custom_css'];
     if (isset($op_options)) {
         wp_add_inline_style('op-style', $op_options);
     }
     wp_register_script('momentjs', plugin_dir_url(__FILE__) . '/js/moment-with-locales.min.js', array('jquery'));
-    wp_register_script('op_order_script', plugin_dir_url(__FILE__) . '/dist/js/order-pop.min.js', array('jquery', 'momentjs'), '1.6');
+    wp_register_script('op_order_script', plugin_dir_url(__FILE__) . '/dist/js/order-pop.min.js', array('jquery', 'momentjs'), '1.6.1');
     wp_localize_script('op_order_script', 'myAjax', array('ajaxurl' => admin_url('admin-ajax.php')));
     wp_enqueue_script('jquery');
     wp_enqueue_script('op_order_script');
@@ -62,8 +62,8 @@ function op_enqueue_assets() {
 
 function op_admin_enqueue_assets() {
     wp_enqueue_media();
-    wp_enqueue_style('op-developer-style', plugin_dir_url(__FILE__) . '/dist/css/op-admin.min.css', array(), '1.6', 'all');
-    wp_enqueue_script('op-developer-script', plugin_dir_url(__FILE__) . '/dist/js/op-admin.min.js', array('jquery'), '1.6', true);
+    wp_enqueue_style('op-developer-style', plugin_dir_url(__FILE__) . '/dist/css/op-admin.min.css', array(), '1.6.1', 'all');
+    wp_enqueue_script('op-developer-script', plugin_dir_url(__FILE__) . '/dist/js/op-admin.min.js', array('jquery'), '1.6.1', true);
 
     //Enqueue CSS just for us
     if (isset($_GET['page']) && $_GET['page'] == 'op_plugin') {
